31eb82599f7c95c6ae17baabfc2ce28af796c5e9
smrityy66 <smrityshrestha734@gmail.com>
2025-01-18 17:32:14 +0545

    Added Main.java with basic structure

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Student Management System!");
+        // Further code to add and manage student records will go here
+    }
 }
